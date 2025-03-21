3cd11645a77d212d1225260ccb316cd2f199b5d2
hafizfauzi02 <hafizfauzi60@gmail.com>
2025-03-22 00:27:30 +0800

    pull this

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: Details1(),
     );
   }
 }
